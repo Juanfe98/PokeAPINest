f427a3298a5cd3c05aeb1c3c91bfb003073314fa
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-10-03 01:19:39 -0500

    Create the configuration to connect the API to the mongo database and create our first entity

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+/**
+ * Entitites usually al related to the tables or documents in the database.
+ */
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
