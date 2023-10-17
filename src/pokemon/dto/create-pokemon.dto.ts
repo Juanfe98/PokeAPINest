675829d58e146ba8b5ba93fe52b158b9a6297f2d
Juan felipe Montaña perdomo <juanmontana1398@gmail.com>
2023-10-17 00:44:08 -0500

    Create PokemonDTO - Add validation and schema constraints to the create pokemon API.
    - Create custom messages based on the error when a constrain faills based in the DTO structure.
    
    - In the service, we are returning the created pokemon.

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,23 @@
-export class CreatePokemonDto {}
+import {
+  IsInt,
+  IsPositive,
+  IsString,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(5, {
+    message:
+      'Title is too short. Minimal length is $constraint1 characters, but actual is $value',
+  })
+  @MaxLength(20)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
